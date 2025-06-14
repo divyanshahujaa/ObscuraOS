4c2c9b4c9514c42f989488320ad86ce3cea8bbe3
Apple <apple@MacBook-Air.local>
2025-06-14 22:48:52 +0530

    Preparing our bootloader to be booted on real hardware

--- boot.asm
+++ boot.asm
@@ -1,7 +1,15 @@
 ORG 0
 BITS 16
+_start:
+    jmp short start
+    nop
+
+times 33 db 0 ; for bios parameter block
 
 start:
+    jmp 0x7c0:step2
+
+step2:
     cli ; clear interrupts
     mov ax, 0x7c0
     mov ds, ax ; data segment
@@ -9,7 +17,7 @@ start:
     mov ax, 0x7c00;
     mov ss, ax
     mov sp, 0
-    
+
     sti ; enable interrupts
     mov si, message
     call print
